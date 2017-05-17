mirokuratczyk/endless

commit fcd6e2471198d45ffcc55c81ec3600b70c111fd2

IASK strings files now use english strings as keys. Other misc localization fixes

Endless/InAppSettings.bundle/MinTLSSettings.plist
@@ -8,7 +8,7 @@
 	<array>
 		<dict>
 			<key>Title</key>
-			<string>TLS_1_2</string>
+			<string>TLS 1.2 only (no SSL)</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>
@@ -22,7 +22,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>TLS_1_1_OR_1_2</string>
+			<string>TLS 1.1 or 1.2 (no SSL)</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>
@@ -34,7 +34,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>TLS_1_0_OR_1_1_OR_1_2</string>
+			<string>TLS 1.0, 1.1, or 1.2 (no SSL)</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>

Endless/InAppSettings.bundle/Security.plist
@@ -14,7 +14,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>HTTPS_EVERYWHERE</string>
+			<string>HTTPS EVERYWHERE</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>
@@ -26,22 +26,22 @@
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 			<key>FooterText</key>
-			<string>MIN_SSL_TLS_VERSION_FOOTER</string>
+			<string>Minimum SSL/TLS Version_FOOTER</string>
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
-			<string>TLS_1_2</string>
+			<string>TLS 1.2 only (no SSL)</string>
 			<key>Values</key>
 			<array>
-				<string>TLS_1_2</string>
+				<string>TLS 1.2 only (no SSL)</string>
 				<string>TLS_1_1</string>
 				<string>TLS_1_0</string>
 			</array>
 			<key>Titles</key>
 			<array>
-				<string>TLS_1_2</string>
-				<string>TLS_1_1_OR_1_2</string>
-				<string>TLS_1_0_OR_1_1_OR_1_2</string>
+				<string>TLS 1.2 only (no SSL)</string>
+				<string>TLS 1.1 or 1.2 (no SSL)</string>
+				<string>TLS 1.0, 1.1, or 1.2 (no SSL)</string>
 			</array>
 			<key>ShortTitles</key>
 			<array>
@@ -52,7 +52,7 @@
 			<key>Key</key>
 			<string>minTlsVersion</string>
 			<key>Title</key>
-			<string>MIN_SSL_TLS_VERSION</string>
+			<string>Minimum SSL/TLS Version</string>
 			<key>Type</key>
 			<string>PSMultiValueSpecifier</string>
 		</dict>

Endless/InAppSettings.bundle/Feedback.plist
@@ -26,7 +26,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>PROXY</string>
+			<string>Proxy</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>

Endless/InAppSettings.bundle/Notifications~ipad.plist
@@ -10,7 +10,7 @@
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 			<key>FooterText</key>
-			<string>SOUND_FOOTER</string>
+			<string>Play sound when connection status changes</string>
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
@@ -18,7 +18,7 @@
 			<key>Key</key>
 			<string>sound_notification</string>
 			<key>Title</key>
-			<string>SOUND</string>
+			<string>Sound</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>

Endless/InAppSettings.bundle/Notifications~iphone.plist
@@ -10,7 +10,7 @@
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 			<key>FooterText</key>
-			<string>SOUND_FOOTER</string>
+			<string>Play sound when connection status changes</string>
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
@@ -18,7 +18,7 @@
 			<key>Key</key>
 			<string>sound_notification</string>
 			<key>Title</key>
-			<string>SOUND</string>
+			<string>Sound</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>
@@ -26,7 +26,7 @@
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 			<key>FooterText</key>
-			<string>VIBRATE_FOOTER</string>
+			<string>Vibrate when connection status changes</string>
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
@@ -34,7 +34,7 @@
 			<key>Key</key>
 			<string>vibrate_notification</string>
 			<key>Title</key>
-			<string>VIBRATE</string>
+			<string>Vibrate</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>

Endless/InAppSettings.bundle/Privacy.plist
@@ -10,7 +10,7 @@
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 			<key>FooterText</key>
-			<string>COOKIE_CONTROL_FOOTER</string>
+			<string>Clear cookies and local storage data from non-whitelisted hosts if not in use for this amount of time</string>
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
@@ -24,22 +24,22 @@
 			</array>
 			<key>Titles</key>
 			<array>
-				<string>MINS_FIFTEEN</string>
-				<string>MINS_THIRTY</string>
-				<string>HOURS_TWELVE</string>
-				<string>HOURS_TWENTYFOUR</string>
+				<string>15 min</string>
+				<string>30 min</string>
+				<string>12 hrs</string>
+				<string>24 hrs</string>
 			</array>
 			<key>ShortTitles</key>
 			<array>
-				<string>MINS_FIFTEEN</string>
-				<string>MINS_THIRTY</string>
-				<string>HOURS_TWELVE</string>
-				<string>HOURS_TWENTYFOUR</string>
+				<string>15 min</string>
+				<string>30 min</string>
+				<string>12 hrs</string>
+				<string>24 hrs</string>
 			</array>
 			<key>Key</key>
 			<string>oldDataSweepMins</string>
 			<key>Title</key>
-			<string>CLEAR_COOKIES</string>
+			<string>Clear cookies if unused for</string>
 			<key>Type</key>
 			<string>PSMultiValueSpecifier</string>
 		</dict>
@@ -47,7 +47,7 @@
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 			<key>FooterText</key>
-			<string>CLEAR_ALL_WHEN_BACKGROUNDED_FOOTER</string>
+			<string>Clear all when backgrounded_FOOTER</string>
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
@@ -55,7 +55,7 @@
 			<key>Key</key>
 			<string>clearAllWhenBackgrounded</string>
 			<key>Title</key>
-			<string>CLEAR_ALL_WHEN_BACKGROUNDED</string>
+			<string>Clear all when backgrounded</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>
@@ -69,7 +69,7 @@
 			<key>Key</key>
 			<string>sendDoNotTrack</string>
 			<key>Title</key>
-			<string>SEND_DO_NOT_TRACK</string>
+			<string>Send &quot;Do-Not-Track&quot; requests</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>

Endless/InAppSettings.bundle/PsiphonSettings.plist
@@ -8,7 +8,7 @@
 	<array>
 		<dict>
 			<key>Title</key>
-			<string>TIMEOUTS</string>
+			<string>Timeouts</string>
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 		</dict>
@@ -18,13 +18,13 @@
 			<key>Key</key>
 			<string>disableTimeouts</string>
 			<key>Title</key>
-			<string>DISABLE_TIMEOUTS</string>
+			<string>Disable Timeouts</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>PROXY</string>
+			<string>Proxy</string>
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 		</dict>
@@ -34,7 +34,7 @@
 			<key>Key</key>
 			<string>useUpstreamProxy</string>
 			<key>Title</key>
-			<string>USE_UPSTREAM_PROXY</string>
+			<string>Use upstream proxy</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>
@@ -42,7 +42,7 @@
 			<key>Key</key>
 			<string>upstreamProxyHostAddress</string>
 			<key>Title</key>
-			<string>HOST_ADDRESS</string>
+			<string>Host address</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>AutocorrectionType</key>
@@ -54,7 +54,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>PORT</string>
+			<string>Port</string>
 			<key>Key</key>
 			<string>upstreamProxyPort</string>
 			<key>Type</key>
@@ -72,7 +72,7 @@
 			<key>Key</key>
 			<string>useProxyAuthentication</string>
 			<key>Title</key>
-			<string>USE_PROXY_AUTHENTICATION</string>
+			<string>Use proxy authentication</string>
 			<key>Type</key>
 			<string>PSToggleSwitchSpecifier</string>
 		</dict>
@@ -80,7 +80,7 @@
 			<key>Key</key>
 			<string>proxyUsername</string>
 			<key>Title</key>
-			<string>PROXY_USERNAME</string>
+			<string>Proxy username</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>AutocorrectionType</key>
@@ -94,7 +94,7 @@
 			<key>IsSecure</key>
 			<true/>
 			<key>Title</key>
-			<string>PROXY_PASSWORD</string>
+			<string>Proxy password</string>
 			<key>Key</key>
 			<string>proxyPassword</string>
 		</dict>
@@ -102,7 +102,7 @@
 			<key>Key</key>
 			<string>proxyDomain</string>
 			<key>Title</key>
-			<string>PROXY_DOMAIN</string>
+			<string>Proxy domain</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>AutocorrectionType</key>

Endless/InAppSettings.bundle/Root.inApp.plist
@@ -6,13 +6,13 @@
 	<array>
 		<dict>
 			<key>Title</key>
-			<string>PSIPHON_AND_BROWSER</string>
+			<string>Psiphon Browser</string>
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>CHANGE_REGION</string>
+			<string>Change Region</string>
 			<key>Key</key>
 			<string>regionSelection</string>
 			<key>Type</key>
@@ -22,7 +22,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>APPLICATION_LANGUAGE</string>
+			<string>Language</string>
 			<key>Key</key>
 			<string>appLanguage</string>
 			<key>Type</key>
@@ -50,7 +50,7 @@
 			</array>
 			<key>Titles</key>
 			<array>
-				<string>DEFAULT_LANGUAGE</string>
+				<string>Default language</string>
 				<string>English</string>
 				<string>فارسی</string>
 				<string>العربية</string>
@@ -72,7 +72,7 @@
 			<key>File</key>
 			<string>PsiphonSettings</string>
 			<key>Title</key>
-			<string>PSIPHON_SETTINGS</string>
+			<string>Psiphon Settings</string>
 			<key>Type</key>
 			<string>PSChildPaneSpecifier</string>
 		</dict>
@@ -80,7 +80,7 @@
 			<key>File</key>
 			<string>Notifications</string>
 			<key>Title</key>
-			<string>NOTIFICATIONS</string>
+			<string>Notifications</string>
 			<key>Type</key>
 			<string>PSChildPaneSpecifier</string>
 		</dict>
@@ -95,14 +95,14 @@
 			</array>
 			<key>Titles</key>
 			<array>
-				<string>DUCKDUCKGO</string>
-				<string>GOOGLE</string>
-				<string>START_PAGE</string>
+				<string>DuckDuckGo</string>
+				<string>Google</string>
+				<string>Startpage</string>
 			</array>
 			<key>Key</key>
 			<string>search_engine</string>
 			<key>Title</key>
-			<string>SEARCH_ENGINE</string>
+			<string>Search Engine</string>
 			<key>Type</key>
 			<string>PSMultiValueSpecifier</string>
 		</dict>
@@ -110,7 +110,7 @@
 			<key>File</key>
 			<string>Privacy</string>
 			<key>Title</key>
-			<string>PRIVACY</string>
+			<string>Privacy</string>
 			<key>Type</key>
 			<string>PSChildPaneSpecifier</string>
 		</dict>
@@ -118,19 +118,19 @@
 			<key>File</key>
 			<string>Security</string>
 			<key>Title</key>
-			<string>SECURITY</string>
+			<string>Security</string>
 			<key>Type</key>
 			<string>PSChildPaneSpecifier</string>
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>HELP</string>
+			<string>Help</string>
 			<key>Type</key>
 			<string>PSGroupSpecifier</string>
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>SUBMIT_FEEDBACK</string>
+			<string>Feedback</string>
 			<key>File</key>
 			<string>Feedback</string>
 			<key>Key</key>
@@ -148,7 +148,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>PRIVACY_POLICY</string>
+			<string>Privacy Policy</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>
@@ -156,7 +156,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>TERMS_OF_USE</string>
+			<string>Terms of Use</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>
@@ -164,7 +164,7 @@
 		</dict>
 		<dict>
 			<key>Title</key>
-			<string>ABOUT</string>
+			<string>About</string>
 			<key>Type</key>
 			<string>IASKCustomViewSpecifier</string>
 			<key>Key</key>

commit 7426fd0453744873157c1ead22be55314066b7c5

Fix corruption of minTlsVersion default value caused by fcd6e2471198d45ffcc55c81ec3600b70c111fd2

Endless/InAppSettings.bundle/MinTLSSettings.plist
@@ -1,49 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>StringsTable</key>
-	<string>Root</string>
-	<key>PreferenceSpecifiers</key>
-	<array>
-		<dict>
-			<key>Title</key>
-			<string>TLS 1.2 only (no SSL)</string>
-			<key>Type</key>
-			<string>IASKCustomViewSpecifier</string>
-			<key>Key</key>
-			<string>tls0</string>
-			<key>IASKTextAlignment</key>
-			<string>IASKUITextAlignmentLeft</string>
-			<key>DefaultValue</key>
-			<true/>
-			<key>ShortTitle</key>
-			<string>TLS 1.2</string>
-		</dict>
-		<dict>
-			<key>Title</key>
-			<string>TLS 1.1 or 1.2 (no SSL)</string>
-			<key>Type</key>
-			<string>IASKCustomViewSpecifier</string>
-			<key>Key</key>
-			<string>tls1</string>
-			<key>IASKTextAlignment</key>
-			<string>IASKUITextAlignmentLeft</string>
-			<key>ShortTitle</key>
-			<string>TLS 1.1+</string>
-		</dict>
-		<dict>
-			<key>Title</key>
-			<string>TLS 1.0, 1.1, or 1.2 (no SSL)</string>
-			<key>Type</key>
-			<string>IASKCustomViewSpecifier</string>
-			<key>Key</key>
-			<string>tls2</string>
-			<key>IASKTextAlignment</key>
-			<string>IASKUITextAlignmentLeft</string>
-			<key>ShortTitle</key>
-			<string>TLS 1.0+</string>
-		</dict>
-	</array>
-</dict>
-</plist>

Endless/InAppSettings.bundle/Security.plist
@@ -30,10 +30,10 @@
 		</dict>
 		<dict>
 			<key>DefaultValue</key>
-			<string>TLS 1.2 only (no SSL)</string>
+			<string>TLS_1_2</string>
 			<key>Values</key>
 			<array>
-				<string>TLS 1.2 only (no SSL)</string>
+				<string>TLS_1_2</string>
 				<string>TLS_1_1</string>
 				<string>TLS_1_0</string>
 			</array>

External/CKHTTPConnection.m
@@ -197,7 +197,7 @@
 						NSLog(@"[Settings] failed setting SSLSetProtocolVersionMin: %d", (int)status);
 
 #ifdef TRACE_HOST_SETTINGS
-					NSLog(@"[Settings] set TLS/SSL min level for %@ to %ld", [url host], (long)tlsVersion);
+					NSLog(@"[Settings] set TLS/SSL min level for %@ to %@", [url host], tlsVersion);
 #endif
 				};
 