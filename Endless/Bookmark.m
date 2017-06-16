b4fdab4a5f9eef67f21b69212fd8dfbfed6807cb
Miro Kuratczyk <miro.kuratczyk@gmail.com>
2017-06-16 12:38:17 -0400

    Added Psiphon3 website to preloaded bookmarks

--- Endless/Bookmark.m
+++ Endless/Bookmark.m
@@ -20,6 +20,7 @@ NSString * const BOOKMARK_KEY_LIST = @"bookmarks";
 const int BOOKMARK_FILE_VERSION = 1;
 
 + (void)addDefaultBookmarks {
+	[Bookmark addBookmarkForURLString:@"https://psiphon3.com" withName:@"Psiphon"];
 	[Bookmark addBookmarkForURLString:@"https://m.facebook.com" withName:@"Facebook"];
 	[Bookmark addBookmarkForURLString:@"https://mobile.twitter.com" withName:@"Twitter"];
 	[Bookmark addBookmarkForURLString:@"https://google.com" withName:@"Google"];
